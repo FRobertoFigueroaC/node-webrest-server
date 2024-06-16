b2b216777409ec7e599a449cb8bc69c57a0d0d64
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-06-16 01:06:27 -0600

    DDD + clean architecture

---
+++ src/presentation/todos/controller.ddd.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from 'express';
+import { prisma } from '../../data/postgres'
+import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos';
+import { TodoRepository } from '../../domain';
+
+
+
+
+export class TodosController {
+  
+  // * DI
+  constructor( private readonly todoRepository:TodoRepository){
+
+  }
+
+  public getTodos =  async(req: Request, res: Response) => {
+    const todos = await this.todoRepository.getAll();
+    return res.json(todos);
+  }
+  public getTodoById =  async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+      return res.status(400).json({error: 'ID argument is not valid'});
+    }
+
+    try {
+      const todo = await this.todoRepository.findById(id);
+      return res.json( todo )
+    } catch (error) {
+      return res.status( 404 ).json( { error: `Todo with id ${ id } not found` } )
+    }
+    
+  }
+
+  public createTodos =  async(req: Request, res: Response) => {
+    const [ error, createTodoDto ] =  CreateTodoDto.create( req.body )
+    if ( error ) return res.status(404).json({error});
+
+    try {
+      const todo = await this.todoRepository.create( createTodoDto! );
+      return res.json( todo );
+    } catch (error) {
+      return res.status(400).json({error});
+    }
+
+  }
+  public updateTodo =  async (req: Request, res: Response) => {
+    const id = Number( req.params.id );
+    const [ error, updateTodoDto ] = UpdateTodoDto.update( { ...req.body, id });
+    if ( error ) return res.status(404).json({error});
+
+    try {
+      const todo = await this.todoRepository.updateTodo( updateTodoDto! );
+      return res.json(todo);
+    } catch (error) {
+      return res.status( 400 ).json( { error } );
+    }
+  }
+
+  public deleteTodo =  async(req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+      return res.status(400).json({error: 'ID argument is not valid'});
+    }
+
+    try {
+      const todo = await this.todoRepository.deleteById(id);
+      return res.json({msg: `Todo with id ${id} deleted`});
+    } catch (error) {
+      return res.status( 400 ).json( { error } );
+    }
+  }
+
+}
--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,74 +1,76 @@
 import { Request, Response } from 'express';
-import { prisma } from '../../data/postgres'
+
 import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos';
-import { TodoRepository } from '../../domain';
+import { CreateTodo, GetTodo, GetTodos,UpdateTodo, TodoRepository, DeleteTodo } from '../../domain';
 
 
 
 
 export class TodosController {
-  
+
   // * DI
-  constructor( private readonly todoRepository:TodoRepository){
+  constructor( private readonly todoRepository: TodoRepository ) {
 
   }
+  /*
+  */
+  public getTodos = ( req: Request, res: Response ) => {
+    new GetTodos(this.todoRepository)
+    .execute()
+    .then(todos => res.json(todos) )
+    .catch(error => res.status(400).json({error}))
+  };
 
-  public getTodos =  async(req: Request, res: Response) => {
-    const todos = await this.todoRepository.getAll();
-    return res.json(todos);
-  }
-  public getTodoById =  async (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      return res.status(400).json({error: 'ID argument is not valid'});
-    }
-
-    try {
-      const todo = await this.todoRepository.findById(id);
-      return res.json( todo )
-    } catch (error) {
-      return res.status( 404 ).json( { error: `Todo with id ${ id } not found` } )
-    }
-    
-  }
-
-  public createTodos =  async(req: Request, res: Response) => {
-    const [ error, createTodoDto ] =  CreateTodoDto.create( req.body )
-    if ( error ) return res.status(404).json({error});
-
-    try {
-      const todo = await this.todoRepository.create( createTodoDto! );
-      return res.json( todo );
-    } catch (error) {
-      return res.status(400).json({error});
-    }
-
-  }
-  public updateTodo =  async (req: Request, res: Response) => {
+  /*
+  */
+  public getTodoById = ( req: Request, res: Response ) => {
     const id = Number( req.params.id );
-    const [ error, updateTodoDto ] = UpdateTodoDto.update( { ...req.body, id });
-    if ( error ) return res.status(404).json({error});
-
-    try {
-      const todo = await this.todoRepository.updateTodo( updateTodoDto! );
-      return res.json(todo);
-    } catch (error) {
-      return res.status( 400 ).json( { error } );
+    if ( isNaN( id ) ) {
+      return res.status( 400 ).json( { error: 'ID argument is not valid' } );
     }
-  }
+    new GetTodo(this.todoRepository)
+      .execute( id )
+      .then( todo => res.json( todo ) )
+      .catch( error => res.status( 404 ).json( { error } ) )
+  };
 
-  public deleteTodo =  async(req: Request, res: Response) => {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      return res.status(400).json({error: 'ID argument is not valid'});
+  /*
+  */
+  public createTodos = ( req: Request, res: Response ) => {
+    const [ error, createTodoDto ] = CreateTodoDto.create( req.body );
+    if ( error ) return res.status( 404 ).json( { error } );
+
+    new CreateTodo(this.todoRepository)
+      .execute( createTodoDto!)
+      .then( todo => res.json( todo ) )
+      .catch( error => res.status( 400 ).json( { error } ) )
+  };
+
+  /*
+  */
+  public updateTodo = ( req: Request, res: Response ) => {
+    const id = Number( req.params.id );
+    const [ error, updateTodoDto ] = UpdateTodoDto.update( { ...req.body, id } );
+    if ( error ) return res.status( 404 ).json( { error } );
+
+    new UpdateTodo(this.todoRepository)
+      .execute( updateTodoDto!)
+      .then( todo => res.json( todo ) )
+      .catch( error => res.status( 400 ).json( { error } ) )
+  };
+
+  /*
+  */
+  public deleteTodo = ( req: Request, res: Response ) => {
+    const id = Number( req.params.id );
+    if ( isNaN( id ) ) {
+      return res.status( 400 ).json( { error: 'ID argument is not valid' } );
     }
 
-    try {
-      const todo = await this.todoRepository.deleteById(id);
-      return res.json({msg: `Todo with id ${id} deleted`});
-    } catch (error) {
-      return res.status( 400 ).json( { error } );
-    }
-  }
+    new DeleteTodo(this.todoRepository)
+    .execute(id)
+      .then( todo => res.json( { msg: `Todo with id ${ id } deleted` } ) )
+      .catch( error => res.status( 404 ).json( { error } ) )
+  };
 
 }
